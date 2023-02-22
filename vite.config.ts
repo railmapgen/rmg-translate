36f17e85536f7a7fe5a264f40fda4e47ff3f8ab4
Chito Wong <chi.to.wong@outlook.com>
2023-02-23 01:46:26 +0800

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config
 export default defineConfig({
     base: '/rmg-translate/',
-    plugins: [checker({ typescript: true, eslint: { lintCommand: 'eslint ./src ./ui' } }), splitVendorChunkPlugin()],
+    plugins: [splitVendorChunkPlugin()],
     build: {
         outDir: 'build',
     },
